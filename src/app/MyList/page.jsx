2bcfc4bfb84364e3f89f8e0bd0748e4a2bba3b74
Anthony Sanchez <antsanchez198@gmail.com>
2023-09-23 23:25:44 -0400

    random movie modal

--- src/app/MyList/page.jsx
+++ src/app/MyList/page.jsx
@@ -1,90 +1,97 @@
 "use client"
-import React, { useEffect, useState } from 'react'
-import styles from './page.module.css'
+import React, { useEffect, useState } from 'react';
+import styles from './page.module.css';
 import MyListCard from '../components/MyListCard';
 import { db } from '../firebase';
-import { getDocs, collection, deleteDoc, doc } from "firebase/firestore";
+import { getDocs, collection, deleteDoc, doc } from 'firebase/firestore';
 
 const MyList = () => {
-
   const [myMovies, setMyMovies] = useState([]);
   const [backupMyMovies, setBackup] = useState([]);
-  const savedMoviesDatabase = collection(db, "my-movie-list");
-  const [randomMovie, setRandomMovie] = useState([])
+  const savedMoviesDatabase = collection(db, 'my-movie-list');
+  const [randomMovie, setRandomMovie] = useState(null); // Changed to null
 
   const getMyMovies = async () => {
     try {
       const data = await getDocs(savedMoviesDatabase);
       const filteredData = data.docs.map((doc) => ({
         ...doc.data(),
-      }))
+      }));
       setMyMovies(filteredData);
       setBackup(filteredData);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-
-  }
-
-  useEffect(() => {
-    const favDialog = document.getElementById("dialog");
-    const closeBtn = document.getElementById("dialog-btn");
-
-    // closeBtn.addEventListener('click', () => {
-    //     favDialog.close();
-    //     document.body.style.overflowY = "auto";
-    // });
-  });
+  };
 
   useEffect(() => {
     getMyMovies();
   }, []);
 
-  useEffect(() => {
-
-  }, [myMovies, randomMovie])
-
   const handleOnSelect = (event) => {
-    let tempArray = []
-    if (event.target.value === "All") {
-      setMyMovies(backupMyMovies)
-    }
-    else {
+    let tempArray = [];
+    if (event.target.value === 'All') {
+      setMyMovies(backupMyMovies);
+    } else {
       backupMyMovies.map((index) => {
         index.services.map((innerIndex) => {
           if (innerIndex.provider_name === event.target.value) {
-            tempArray.push(index)
+            tempArray.push(index);
           }
-        })
-      })
+        });
+      });
       setMyMovies(tempArray);
     }
-    setRandomMovie([])
-  }
+    setRandomMovie(null); // Changed to null
+  };
 
   const deleteMovie = async (id) => {
-    const movieDoc = doc(db, "my-movie-list", id.toString());
+    const movieDoc = doc(db, 'my-movie-list', id.toString());
     await deleteDoc(movieDoc);
     getMyMovies();
-  }
+  };
 
   const randomMoviePicker = () => {
     if (myMovies.length > 1) {
-      setRandomMovie(myMovies.at(Math.floor(Math.random() * myMovies.length)));
-      console.log(myMovies.at(Math.floor(Math.random() * myMovies.length)))
+      const randomIndex = Math.floor(Math.random() * myMovies.length);
+      setRandomMovie(myMovies[randomIndex]);
     }
+  };
+
+  // Define a function to close the modal
+  const closeModal = () => {
+    setRandomMovie(null);
+    const bodyElement = document.getElementById("body");
+    if (bodyElement) {
+      bodyElement.style.overflow = "hidden";
+    }
+    const modalContainer = document.getElementById("modal-container");
+    if (modalContainer) {
+      modalContainer.classList.remove('scale-100', 'opacity-100');
+      modalContainer.classList.add('scale-50', 'opacity-0');
+    };
+    requestAnimationFrame(() => {
+      modalContainer.classList.add('transform-gone', 'transition-transform');
+    });
   }
 
-  return (
-    <div className="">
-      <div className=" text-black flex flex-col my-10 mx-auto max-w-screen-xl xl:my-0 p-10">
-        <button className="
-      bg-gradient-to-r from-fuchsia-500 to-cyan-500 w-1/4 self-center p-4 text-slate-100 rounded-xlg font-semibold text-xl rounded-2xl shadow-lg
-      hover:bg-gradient-to-tr	from-fuchsia-500 to-cyan-500 hover:transition duration-300 ease-in-out hover:shadow-2xl"
-          onClick={() => randomMoviePicker()}
-        >Pick Random Movie</button>
+    return (
+      <div className=" text-black flex flex-col my-10 mx-auto max-w-screen-xl xl:my-0 p-10" id="body">
+        <button
+          id="showMovie"
+          className="bg-gradient-to-r from-fuchsia-500 to-cyan-500 w-1/4 self-center p-4 text-slate-100 rounded-xlg font-semibold text-xl rounded-2xl shadow-lg
+          hover:bg-gradient-to-tr	from-fuchsia-500 to-cyan-500 hover:transition duration-300 ease-in-out hover:shadow-2xl"
+          onClick={randomMoviePicker}
+        >
+          Pick Random Movie
+        </button>
         <form>
-          <select name="cars" id="cars" onChange={(e) => handleOnSelect(e)} className="bg-slate-500 w-45 px-1 py-2 text-slate-50 text-lg mt-20 cursor-pointer">
+          <select
+            name="cars"
+            id="cars"
+            onChange={(e) => handleOnSelect(e)}
+            className="bg-slate-500 w-45 px-1 py-2 text-slate-50 text-lg mt-20 cursor-pointer"
+          >
             <option value="All">All</option>
             <option value="Netflix">Netflix</option>
             <option value="Disney Plus">Disney Plus</option>
@@ -99,31 +106,46 @@ const MyList = () => {
             <option value="Sling">Sling</option>
           </select>
         </form>
-        <div className="mt-10 grid grid-cols-1 gap-10 md:grid-cols-2 xl:grid-cols-4 ">
-          {randomMovie.length < 1 ? myMovies && myMovies.map(index => {
-            return (
+        {/* <div className="mt-10 grid grid-cols-1 gap-10 md:grid-cols-2 xl:grid-cols-4"> */}
+        <div className="w-full h-fit text-center flex flex-wrap">
+          {randomMovie === null ? (
+            myMovies.map((index) => (
+              // <div className="w-fit" key={index.movieID}>
               <MyListCard
+                key={index.movieID}
                 title={index.title}
                 image={index.poster}
                 date={index.date}
                 services={index.services}
                 deleteFunction={() => deleteMovie(index.movieID)}
               />
-            )
-          }
-          ) :
-            <MyListCard
-              title={randomMovie?.title}
-              image={randomMovie?.poster}
-              date={randomMovie?.date}
-              services={randomMovie?.services}
-              deleteFunction={() => deleteMovie(randomMovie?.movieID)}
-            />
-          }
+              // </div>
+            ))
+          ) : (
+            <div
+              className="fixed z-[1] w-full h-full bg-transparent pt-[100px] left-0 -top-10"
+              onClick={closeModal}
+            >
+              <div
+                className="bg-gradient-to-r from-fuchsia-500 to-cyan-500 w-1/2 mx-auto my-0 p-5 rounded-lg duration-500"
+                id="modal-container">
+                <div className="w-1/2 h-fit mx-auto my-0 p-5">
+                  <h2 className="text-slate-100 rounded-xlg font-semibold text-3xl mb-10">Movie Selected</h2>
+                  <MyListCard
+                    title={randomMovie.title}
+                    image={randomMovie.poster}
+                    date={randomMovie.date}
+                    services={randomMovie.services}
+                    deleteFunction={() => deleteMovie(randomMovie.movieID)}
+                  />
+                </div>
+              </div>
+            </div>
+
+          )}
         </div>
       </div>
-    </div>
-  )
-}
+    );
+  };
 
-export default MyList
+  export default MyList;
--- src/app/components/MyListCard.js
+++ src/app/components/MyListCard.js
@@ -1,44 +1,57 @@
-import React from 'react'
-import styles from './listCard.module.css'
+import React from 'react';
+import styles from './listCard.module.css';
 
 const MyListCard = (props) => {
     return (
-        <div className="rounded-xl backdrop-blur-sm bg-white/30 h-50 w-fit text-center shadow-lg font-sans flex flex-col justify-between hover:shadow-xl">
-            <img src={`https://image.tmdb.org/t/p/original${props.image}`} className="w-auto basis-1/2 rounded-t-xl drop-shadow-xl" />
-            <div className="my-4 flex justify-center items-center flex-col gap-5 basis-1/2">
-                <h3 className="text-xl font-bold text-gray-800">{props.title}</h3>
-                <div className="flex flex-row w-fit gap-2">
-                    {props?.services?.length > 0 ?
-                        props.services && props.services.map(index => {
-                            return (
-                                <img src={`https://image.tmdb.org/t/p/original${index.logo_path}`} className="w-11 h-11 rounded-3xl object-contain shadow-md" />
-                            )
-                        }
-                        )
-                        :
-                        <div className="font-normal">No Streaming Platforms</div>
-                    }
+        // <div className="rounded-xl backdrop-blur-sm bg-white/30 h-fit w-fit text-center shadow-lg font-sans hover:shadow-xl">
+        <div className="flex-[0] w-[35%] min-w-[250px] h-[350px] m-[1em] bg-white/50 rounded-xl">
+            {/* <img src={`https://image.tmdb.org/t/p/original${props.image}`} className="w-auto h-16 rounded-t-xl drop-shadow-xl" alt={props.title} /> */}
+            <div className="h-1/2 relative">
+                <img
+                    src={`https://image.tmdb.org/t/p/original${props.image}`}
+                    className="w-full h-full object-cover rounded-t-xl drop-shadow-xl"
+                    alt={props.title}
+                />
+            </div>
+            <div className="my-4 flex flex-col items-center gap-2">
+                <h3 className="text-xl font-semibold text-gray-800">{props.title}</h3>
+                <div className="flex flex-row gap-2">
+                    {props?.services?.length > 0 ? (
+                        props.services.map((index) => (
+                            <img
+                                key={index.logo_path}
+                                src={`https://image.tmdb.org/t/p/original${index.logo_path}`}
+                                className="w-11 h-11 rounded-3xl object-contain shadow-md"
+                                alt={index.provider_name}
+                            />
+                        ))
+                    ) : (
+                        <div className="font-light text-normal">No Streaming Platforms</div>
+                    )}
                 </div>
-                <div className='flex flex-row gap-3 items-center justify-center'>
-                    <button className="border-green-600	border-solid border-2 bg-green-200 rounded-2xl px-3 py-1 w-fit self-center cursor-pointer text-green-600 font-semibold shadow-md 
-                hover:text-slate-100 hover:bg-green-600 text-white-800 hover:border-white-800 hover:transition duration-300 ease-in-out hover:shadow-xl "
+                <div className="flex flex-row gap-3 items-center">
+                    <button
+                        className="border-green-600 border-solid border-2 bg-green-200 rounded-2xl px-3 py-1 w-fit cursor-pointer text-green-600 font-semibold shadow-md hover:text-slate-100 hover:bg-green-600 text-white-800 hover:border-white-800 hover:transition duration-300 ease-in-out hover:shadow-xl"
                         onClick={props.deleteFunction}
-                    ><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-thumb-up" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-thumb-up" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                             <path d="M7 11v8a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1v-7a1 1 0 0 1 1 -1h3a4 4 0 0 0 4 -4v-1a2 2 0 0 1 4 0v5h3a2 2 0 0 1 2 2l-1 5a2 3 0 0 1 -2 2h-7a3 3 0 0 1 -3 -3"></path>
-                        </svg></button>
-                    <button className="border-red-600 bg-red-200 border-solid border-2 rounded-2xl px-3 py-1 w-fit self-center cursor-pointer text-red-600 font-semibold shadow-md 
-                hover:text-slate-100 hover:bg-red-600 text-white-800 hover:transition duration-300 ease-in-out hover:shadow-xl "
+                        </svg>
+                    </button>
+                    <button
+                        className="border-red-600 bg-red-200 border-solid border-2 rounded-2xl px-3 py-1 w-fit cursor-pointer text-red-600 font-semibold shadow-md hover:text-slate-100 hover:bg-red-600 text-white-800 hover:transition duration-300 ease-in-out hover:shadow-xl"
                         onClick={props.deleteFunction}
-                    ><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-thumb-down" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-thumb-down" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                             <path d="M7 13v-8a1 1 0 0 0 -1 -1h-2a1 1 0 0 0 -1 1v7a1 1 0 0 0 1 1h3a4 4 0 0 1 4 4v1a2 2 0 0 0 4 0v-5h3a2 2 0 0 0 2 -2l-1 -5a2 3 0 0 0 -2 -2h-7a3 3 0 0 0 -3 3"></path>
-                        </svg></button>
+                        </svg>
+                    </button>
                 </div>
             </div>
         </div>
-        
-    )
+    );
 }
 
-export default MyListCard
+export default MyListCard;
